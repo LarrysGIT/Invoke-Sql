5b89c4c566335dc836e54629500be72feecfe16d
Larry Song <Larry.Song@civica.com.au>
2019-06-28 09:46:19 +1000

    first commit

---
+++ Invoke-Sql.ps1
@@ -0,0 +1,34 @@
+function Invoke-Sql(){
+    PARAM(
+        [string]$Query,
+        [string]$ServerInstance,
+        [string]$Database = $null,
+        [string]$Username = $null,
+        [string]$Password = $null,
+        [string]$ConnectionString = $null,
+        [int]$ConnectionTimeout = 10,
+        [int]$QueryTimeout = 30
+    )
+    if($ConnectionString)
+    {
+        $sqlConnectionString = $ConnectionString
+    }
+    else
+    {
+        $sqlConnectionString = "Server=$ServerInstance"
+        if($Database){$sqlConnectionString += ";Database=$Database"}
+        if($Username){$sqlConnectionString += ";User Id=$Database; Password=$Password"}
+                 else{$sqlConnectionString += ";Trusted_Connection=True"}
+        if($ConnectionTimeout){$sqlConnectionString += ";Connect Timeout=$ConnectionTimeout"}
+    }
+    $sql_Conn = New-Object System.Data.SqlClient.SQLConnection
+    $sql_Conn.ConnectionString = $sqlConnectionString
+    $sql_Conn.Open()
+    $sql_cmd = New-Object system.Data.SqlClient.SqlCommand($Query, $sql_Conn)
+    $sql_cmd.CommandTimeout = $QueryTimeout
+    $sql_ds = New-Object system.Data.DataSet
+    $sql_da = New-Object system.Data.SqlClient.SqlDataAdapter($sql_cmd)
+    [void]$sql_da.fill($sql_ds)
+    $sql_Conn.Close()
+    return $sql_ds
+}
