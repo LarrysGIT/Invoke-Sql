8905962aa5a330341c7b9c54673570bb57849e41
Larry Song <Larry.Song@civica.com.au>
2019-10-29 15:25:13 +1100

    bulk copy data

--- Invoke-Sql.ps1
+++ Invoke-Sql.ps1
@@ -1,3 +1,4 @@
+
 function Invoke-Sql(){
     PARAM(
         [string]$Query,
@@ -36,4 +37,127 @@ function Invoke-Sql(){
     [void]$sql_da.fill($sql_ds)
     $sql_Conn.Close()
     return $sql_ds
+}
+
+function Get-TableDefination
+{
+    PARAM(
+        [string]$ServerInstance,
+        [string]$Database,
+        [string]$UserName,
+        [string]$Password,
+        [string]$ConnectionString,
+        [string]$TableNamePattern
+    )
+
+    if(!$ConnectionString)
+    {
+        $ConnectionString = "Server=$ServerInstance;Database=$Database;"
+        if($UserName)
+        {
+            $ConnectionString += ";User Id=$UserName; Password=$Password"
+        }
+        else
+        {
+            $ConnectionString += ";Trusted_Connection=True"
+        }
+    }
+
+    class objTable {
+        [string]$TableName
+        [string]$TableCreationScript
+    }
+
+    Import-Module SQLPS
+    $Error.Clear()
+    $options = New-Object -TypeName Microsoft.SqlServer.Management.Smo.ScriptingOptions
+    $options.DriAll = $true
+    $options.SchemaQualify = $true
+
+    $connection = New-Object -TypeName Microsoft.SqlServer.Management.Common.ServerConnection
+    $connection.ConnectionString = $ConnectionString
+    $server = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server $connection
+    $server.Databases.Item($Database).Tables | ?{$_.Name -imatch $TableNamePattern} | %{
+        New-Object objTable -Property @{TableName = $_.Name; TableCreationScript = $_.Script($options)}
+    }
+    $connection.Disconnect()
+    $connection = $null
+    $server = $null
+}
+
+function Invoke-DTSWizard
+{
+    PARAM(
+        [string]$SourceInstance,
+        [string]$SourceDatabase,
+        [string]$SourceUserName,
+        [string]$SourcePassword,
+        [string]$SourceConnectionString,
+        [string]$TableNamePattern,
+        [string]$DestinationInstance,
+        [string]$DestinationDatabase,
+        [string]$DestinationUserName,
+        [string]$DestinationPassword,
+        [string]$DestinationConnectionString
+    )
+
+    # Build source connection string
+    if(!$SourceConnectionString)
+    {
+        $SourceConnectionString = "Server=$SourceInstance;Database=$SourceDatabase;"
+        if($SourceUserName)
+        {
+            $SourceConnectionString += ";User Id=$SourceUserName; Password=$SourcePassword"
+        }
+        else
+        {
+            $SourceConnectionString += ";Trusted_Connection=True"
+        }
+    }
+
+    # Build destination connection string
+    if(!$DestinationConnectionString)
+    {
+        $DestinationConnectionString = "Server=$DestinationInstance;Database=$DestinationDatabase;"
+        if($DestinationUserName)
+        {
+            $DestinationConnectionString += ";User Id=$DestinationUserName; Password=$DestinationPassword"
+        }
+        else
+        {
+            $DestinationConnectionString += ";Trusted_Connection=True"
+        }
+    }
+
+    function private:BulkCopyTable()
+    {
+        PARAM(
+            [string]$SourceConnectionString,
+            [string]$DestinationConnectionString,
+            [string]$TableName
+        )
+        $SourceConnection = New-Object System.Data.SqlClient.SQLConnection($SourceConnectionString)
+        $SourceSqlCommand = New-Object system.Data.SqlClient.SqlCommand("Select * From $TableName;", $SourceConnection)
+        $SourceConnection.Open()
+        [System.Data.SqlClient.SqlDataReader]$SourceSqlReader = $SourceSqlCommand.ExecuteReader()
+
+        $DestinationBulkCopy = New-Object Data.SqlClient.SqlBulkCopy($DestinationConnectionString, [System.Data.SqlClient.SqlBulkCopyOptions]::KeepIdentity)
+        $DestinationBulkCopy.DestinationTableName = $TableName
+        $DestinationBulkCopy.WriteToServer($SourceSqlReader)
+        
+        $SourceConnection.Close()
+        $DestinationBulkCopy.Close()
+    }
+
+    $Tables = Get-TableDefination -ServerInstance $SourceInstance -Database $SourceDatabase -TableNamePattern $TableNamePattern -ConnectionString $SourceConnectionString
+    foreach($Table in $Tables)
+    {
+        Write-Host "Copying $($Table.TableName)"
+        # drop destination table
+        Invoke-Sql -Query "Drop table if exists $($Table.TableName);" -Database $DestinationDatabase -ServerInstance $DestinationInstance
+        # create destination table
+        Invoke-Sql -Query $Table.TableCreationScript -Database $DestinationDatabase -ServerInstance $DestinationInstance
+        # CopyTable
+        BulkCopyTable -SourceConnectionString $SourceConnectionString -DestinationConnectionString $DestinationConnectionString -TableName $Table.TableName
+    }
 }
--- README.md
+++ README.md
@@ -2,23 +2,40 @@
 A replacement when invoke-sqlcmd cmdlet is not available
 
 ## Invoke-Sql
-    * Is able to handle the key separator `GO` (by replacing, maybe there is unknown bug)
+    * Is able to handle the key separator `GO` (by regular expression replacing, maybe there is an unknown bug)
     * Is able to handle duplicate columns
     * Fully support multiple result sets
-    * Unable to handle `Create or alter` key words if there are contents ahead
-    * Unable to handle special characters like `194 160` (non-breaking space) in SQL script (edited by some document edit tool, MS word for example)
+    * Unable to handle 'Create or alter' key words
+    * Unable to handle special characters like '194 160' (non-breaking space) in SQL script (edited by some document edit tool, MS word for example)
 
 ## Invoke-SqlCmd
-    * Is able to handle the key separator `GO`
-    * Is able to handle special characters like `non-breaking space`
+    * Is able to handle the key separator 'GO'
+    * Is able to handle special characters like 'non-breaking space'
     * Unable to handle duplicate columns
-    * Unable to fully handle multiple result sets (when first table is empty)
+    * Unable to fully handle multiple result sets (when first set is empty, nothing will be returned)
 
 ## sqlcmd.exe
     * Is able to handle all things
     * The returned result sets are plain text, hard to parse
 
 ## `Microsoft.SqlServer.SMO`
-    * The API of SQL server management studio
-    * Theoratically should be able to handle all cases
-    * Need dig more
+    * Brief tested. 
+    * Seems not able to handle 'GO'.
+    * The ability to handle special characters like 'non-breaking space' needs to be tested
+
+# DTSWizard.exe
+The SQL Server data import/export tools is usefully sometimes. Unfortunately, there is no straight way to automate it
+
+So, some new commands introduced here to emulate DTSWizard.exe
+
+# Get-TableDefination
+Generate a table and its defination,
+
+ - Require SQL management studio installed because script uses API https://docs.microsoft.com/en-us/sql/powershell/load-the-smo-assemblies-in-windows-powershell?view=sql-server-ver15
+ - Same activity in SQL management studio, right click on table -> script table as -> create to
+
+# Invoke-DTSWizard
+A replacement to automate DTSWizard.exe, uses the .NET API `Data.SqlClient.SqlBulkCopy`
+
+ - The command will `drop` any existing tables in destination
+ - The parameter `TableNamePattern` is regular expression, use with caution
